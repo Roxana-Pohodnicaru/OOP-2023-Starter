c06da4caabb6570c2e6b7f133e097943999cc881
roxi-26 <roxypohodnicaru@gmail.com>
2023-04-26 17:03:34 +0100

    added loadFile and findWord fxns

--- java/src/ie/tudublin/DANI.java
+++ java/src/ie/tudublin/DANI.java
@@ -1,12 +1,15 @@
 package ie.tudublin;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.ArrayList;
 
 import processing.core.PApplet;
 
 public class DANI extends PApplet {
 
-	
+	ArrayList<Follow> follows = new ArrayList<Follow>();
 
 	public void settings() {
 		size(1000, 1000);
@@ -14,6 +17,63 @@ public class DANI extends PApplet {
 	}
 
     String[] sonnet;
+	String word = " ";
+	int count = 0;
+
+
+	public void findWord(Follow word)
+	{
+		// iterate through array list
+		// if word matches next word, count ++
+		for (Follow f:follows)
+		{
+			word = f;
+
+			if(follows.contains(word))
+			{
+				count++;
+			}	
+		}
+	}
+
+
+	public void loadFile()
+	{
+		BufferedReader input = null;
+
+		try {
+
+			input = new BufferedReader(new FileReader("small.txt"));
+
+			
+
+			// read entire file
+			while((word = input.readLine()) != null)
+			{
+				// add word to array list
+			}
+
+
+		}
+		catch (IOException e)
+		{
+			e.printStackTrace();
+		}
+		finally
+		{
+			if (input != null)
+			{
+				try {
+					input.close();
+				}
+				catch(Exception e)
+				{
+					e.printStackTrace();
+				}
+			}
+		}
+
+	}
 
     public String[] writeSonnet()
     {
