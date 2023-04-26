2dcf3c98ddb643bffba925944e6d6a0dbf245121
roxi-26 <roxypohodnicaru@gmail.com>
2023-04-26 16:12:14 +0100

    made new class 'word.java', declared fields

---
+++ java/src/ie/tudublin/Word.java
@@ -0,0 +1,14 @@
+package ie.tudublin;
+
+import java.util.ArrayList;
+
+public class Word {
+    
+    public String word = " ";
+
+    ArrayList<Follow> follows = new ArrayList<Follow>();
+
+    
+    
+
+}
